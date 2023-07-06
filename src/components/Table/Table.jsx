e6a019c34830a05b8cf649a7b37828892b8b5f3e
sallyman128 <salmaan.arif.ali@gmail.com>
2023-07-06 16:00:52 -0400

    create a table component to show cards

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const Table = ({ elements }) => {
+  // Calculate the number of rows needed
+  const numRows = Math.ceil(elements.length / 3);
+
+  // Create an array of rows
+  const rows = Array.from({ length: numRows }, (_, index) => {
+    const startIndex = index * 3;
+    const endIndex = startIndex + 3;
+    const rowElements = elements.slice(startIndex, endIndex);
+
+    // Render the Card components for each row
+    const cards = rowElements.map((element, idx) => (
+      <td key={idx}>
+        <Card
+          imageSrc={element.imageSrc}
+          description={element.description}
+        />
+      </td>
+    ));
+
+    return <tr key={index}>{cards}</tr>;
+  });
+
+  return (
+    <table>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+export default Table;
