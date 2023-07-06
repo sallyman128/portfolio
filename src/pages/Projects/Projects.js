95a16ea0c4448309505da5bf0ce6c1e937cd9007
sallyman128 <salmaan.arif.ali@gmail.com>
2023-07-06 16:01:09 -0400

    implement showing cards in table

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,39 @@
 import React from "react";
+import "./Projects.css"
+import Card from "../../components/Card/Card";
+import giraffe from  "../../assets/giraffe.jpg"
+import Table from "../../components/Table/Table";
+
 
 const Projects = () => {
+
+  const projects = [
+    {
+      imageSrc: giraffe,
+      description: "fdafda"
+    },
+    {
+      imageSrc: giraffe,
+      description: "fdafda"
+    },
+    {
+      imageSrc: giraffe,
+      description: "fdafda"
+    },
+    {
+      imageSrc: giraffe,
+      description: "fdafda"
+    },
+    {
+      imageSrc: giraffe,
+      description: "fdafda"
+    }
+  ]
+
   return (
     <div>
-      Projects
+      <h1>Projects</h1>
+      <Table elements={projects} />
     </div>
   )
 }
