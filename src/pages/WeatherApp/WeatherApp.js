43cb3bb61e4f71b3c3343c4a6c7b9fcf53d2eb71
sallyman128 <salmaan.arif.ali@gmail.com>
2023-09-30 14:26:48 -0400

    created weather component

---
+++ src/pages/WeatherApp/WeatherApp.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const WeatherApp = () => {
+    const [location, setLocation] = useState("")
+    const [weather, setWeather] = useState()
+
+    const updateLocation = (e) => {
+        setLocation(e.target.value)
+    }
+
+    const getWeather = () => {
+        //fetch weather from api given the city
+        setWeather("fetched weather")
+        setLocation("")
+    }
+
+    return (
+        <div className="center-horizontal">
+            <h1>Enter your location to get the weather</h1>
+            <input type="text" 
+                placeholder="Enter location" 
+                maxLength={50} 
+                className="center-horizontal" 
+                value={location}
+                onChange={updateLocation}
+            />
+            <fieldset>
+                <legend>Select units:</legend>
+                <div>
+                    <input type="radio" id="farenheight" name="weather" value="farenheight" />
+                    <label htmlFor="farenheight">Farenheight</label>
+                </div>
+                <div>
+                    <input type="radio" id="celsius" name="weather" value="celsius" />
+                    <label htmlFor="celsius">Celsius</label>
+                </div>
+            </fieldset>
+            <button onClick={getWeather}>Get Weather</button>
+            <div>
+                {weather ? `${location}` : ""}
+            </div>
+        </div>
+    )
+}
+
+export default WeatherApp
