962db7fef84a7024369878bddc2b8ecc94511882
Salmaan Ali <salmaan.arif.ali@gmail.com>
2024-05-04 01:15:42 -0400

    add a navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom"
 
 import RoutePaths from './RoutePaths';
-import Sidebar from "./components/Sidebar/Sidebar"
-
+// import Sidebar from "./components/Sidebar/Sidebar"
+import NavBar from './components/NavBar';
 import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Sidebar>
+      <NavBar>
         <RoutePaths />
-      </Sidebar>
+      </NavBar>
     </BrowserRouter>
   );
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { Navbar } from "react-bootstrap"
+import {Container} from "react-bootstrap"
+
+const NavBar = () => {
+    return (
+        <Navbar>
+            <Container>
+                
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
